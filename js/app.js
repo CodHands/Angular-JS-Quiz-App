93ee085ed6b082afb619d25988d3c846a8f3e629
Vinay Singh <vinaysinghbb@gmail.com>
2017-01-06 03:32:19 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+/*
+ * IIFE to keep code clean and safe
+ */
+(function(){
+
+    /*
+     * Declaration of main angular module for this appllication.
+     *
+     * It is named turtleFacts and has no dependencies (hence the 
+     * empty array as the second argument)
+     */
+    angular
+        .module("turtleFacts", []);
+
+})();
